8fbd14694aec8d3363fd7d6e7dbb1c9ad8f12842
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-08-09 00:26:03 +0200

    Update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,39 @@
 function pascal_triangle(n) {
+    // If n < or = to 0, return an empty array
     if (n <= 0) {
         return [];
     }
-    function createRow(index) {
-        if (index === 0) {
+
+    // Recursive function to generate row at given idx
+    function createRow(idx) {
+        // Base case: The first row is always [1]
+        if (idx === 0) {
             return [1];
         }
 
-        const previousLevel = createRow(index - 1);
+        //  get the previous row (Recursive)
+        const previousLevel = createRow(idx - 1);
+        // Start current row with 1
         const currentRow = [1];
 
+        // fill the current row with sum adjacent elements in the previous row
         for (let k = 1; k < previousLevel.length; k++) {
             currentRow.push(previousLevel[k - 1] + previousLevel[k]);
         }
 
+        // End current row with a 1
         currentRow.push(1);
         return currentRow;
     }
 
+    // Initialize empty array to hold the whole Triangle
     const pascalTriangle = [];
+
+    // Generate each row  add it to the Triangle
     for (let i = 0; i < n; i++) {
         pascalTriangle.push(createRow(i));
     }
 
+    // Return complete Triangle
     return pascalTriangle;
 }
