c7b596b1ac8cbbaad0763aedfe5f3fb4df665619
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-08-11 18:51:30 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,14 +5,14 @@ def pascal_triangle(n):
     if n <= 0:
         return []
       
-    Ptriangle = []
+    ptriangle = []
 
     for i in range(n):
         row = [1] * (i + 1)
 
         for j in range(1, i):
-            row[j] = Ptriangle[i-1][j-1] + Ptriangle[i-1][j]
+            row[j] = ptriangle[i-1][j-1] + ptriangle[i-1][j]
 
-        Ptriangle.append(row)
+        ptriangle.append(row)
 
-    return Ptriangle
+    return ptriangle
