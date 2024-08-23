16ba841e603f64c99da9ddabc4a8a4857c14697b
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-08-23 10:34:33 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-  """
-  function determining if all boxes can be opened.
-  Args:
+
+"""
+Function determining if all boxes can be opened.
+
+Args:
     boxes: A list of lists of boxes and corresponding keys.
-  Returns:
+
+Returns:
     True if all boxes can be opened, False if otherwise.
-  """
+"""
+
+
 def canUnlockAll(boxes):
-  box_n = len(boxes)
-  opened_boxes = set([0])
-  keys = set(boxes[0])
+    box_n = len(boxes)
+    opened_boxes = set([0])
+    keys = set(boxes[0])
 
-  while keys:
-    temp_keys = set()
-    for key in keys:
-      if key < box_n and key not in opened_boxes:
-        opened_boxes.add(key)
-        temp_keys.update(boxes[key])
-    keys = temp_keys
+    while keys:
+        temp_keys = set()
+        for key in keys:
+            if key < box_n and key not in opened_boxes:
+                opened_boxes.add(key)
+                temp_keys.update(boxes[key])
+        keys = temp_keys
 
-  return len(opened_boxes) == box_n
+    return len(opened_boxes) == box_n
