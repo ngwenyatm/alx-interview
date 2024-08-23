3b368616a4f3d75d58ecd23aa2ad9a6cbdcbbe06
ThabsNspaces <42273431+ngwenyatm@users.noreply.github.com>
2024-08-23 10:44:05 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,23 @@
-#!/usr/bin/python3
-  """
-  Returns a list of lists representing Pascal's triangle of n rows.
-  """
+#!/usr/bin/env python3
+"""
+Returns a list of lists representing Pascal's triangle of n rows.
+"""
+
 
 def pascal_triangle(n):
-  """Returns n rows of Pascal's triangle."""
+    """Returns n rows of Pascal's triangle."""
 
-  if n <= 0:
-    return []
+    if n <= 0:
+        return []
 
-  pascal_triangle = []
-  for row_num in range(n):
-    row = [1] * (row_num + 1)
-    for col_num in range(1, row_num):
-      row[col_num] = pascal_triangle[row_num - 1][col_num - 1] + pascal_triangle[row_num - 1][col_num]
-    pascal_triangle.append(row)
+    pascal_triangle = []
+    for row_num in range(n):
+        row = [1] * (row_num + 1)
+        for col_num in range(1, row_num):
+            row[col_num] = (
+                pascal_triangle[row_num - 1][col_num - 1]
+                + pascal_triangle[row_num - 1][col_num]
+            )
+        pascal_triangle.append(row)
 
-  return pascal_triangle
+    return pascal_triangle
