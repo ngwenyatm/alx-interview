4735f3537ba8d0cfe631b72044ec55ad78b79d5c
ngwenyatm <thabsnspaces@gmail.com>
2024-10-03 20:48:22 +0200

    pushed with script

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,6 +5,7 @@ Given a pile of coins of different values, determine the fewest number of coins 
 """
 
 def makeChange(coins, total):
+    
     """
     Calculates the fewest number of coins needed to meet a given total amount.
 
@@ -15,13 +16,11 @@ def makeChange(coins, total):
     Returns:
         The fewest number of coins needed to meet the total amount, or else returns -1.
     """
-    if total == 0:
+
+    if total <= 0:
         return 0
-    dp = {0: 0}  # Initialize DP table with base case
+    dp = [0] + [float("inf")] * (total)
     for coin in coins:
         for i in range(coin, total + 1):
-            if i in dp:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-            else:
-                dp[i] = float('inf')
-    return dp[total] if dp[total] != float('inf') else -1
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
