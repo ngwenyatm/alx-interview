fc494144eb68cf9d19a56e7f5612c21ca4d1bd6d
Thabiso <42273431+ngwenyatm@users.noreply.github.com>
2024-12-12 16:25:41 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,38 +12,37 @@ def isWinner(x, nums):
     Returns:
         str: "Maria" if Maria wins more rounds, "Ben" if Ben wins more rounds, or None if it is a tie.
     """
-    
-    if not nums or x < 1:
-        return None
-    
-    max_num = max(nums)
 
-    prime_filter = [True for _ in range(max(max_num + 1, 2))]
-    
+    if not isinstance(x, int) or x < 1:
+        return None
+    if not isinstance(nums, list) or not all(isinstance(num, int) and num >= 2 for num in nums):
+        return None
+
+    max_num = max(nums)
+    prime_fil = [True for _ in range(max(max_num + 1, 2))]
+
     for i in range(2, int(pow(max_num, 0.5)) + 1):
-        if not prime_filter[i]:
+        if not prime_fil[i]:
             continue
         for j in range(i * i, max_num + 1, i):
-            prime_filter[j] = False
-    
-    prime_filter[0] = prime_filter[1] = False
-    
+            prime_fil[j] = False
+
+    prime_fil[0] = prime_fil[1] = False
+
     prime_count = 0
     for i in range(len(prime_filter)):
-        if prime_filter[i]:
+        if prime_fil[i]:
             prime_count += 1
-        prime_filter[i] = prime_count
+        prime_fil[i] = prime_count
 
     maria_wins = 0
-    
     for num in nums:
-        if prime_filter[num] % 2 == 1:
+        if prime_fil[num] % 2 == 1: 
             maria_wins += 1
-    
+
     if maria_wins * 2 == len(nums):
         return None
     elif maria_wins * 2 > len(nums):
         return "Maria"
     else:
         return "Ben"
-
